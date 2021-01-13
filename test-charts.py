2a1785310804f17f0b1eaae9efff8aa243f778d0
Milos <milosvuksanovic53@gmail.com>
2021-01-13 19:16:38 +1100

    Added  file - 2 hold my ideas and experiment them.

---
+++ test-charts.py
@@ -0,0 +1,24 @@
+import yfinance as yf
+import pandas as  pd
+import numpy as np
+import matplotlib.pyplot as plot
+
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+
+#################################################
+#XRO CHART | ARB CHART | BTC CHART
+#TSLA CHART | N/A....  | N/A
+#################################################
+
+#Quick note to self:
+#The charts can even be arranged like this:
+#  | XRO CHART
+#  | ARB CHART
+#  | BTC CHART
+#  | TSLA CHART 
+
+#The command line holds info such as Close and date (UTC Format) for prices of each stock.
+
+print("this is an added file which works as a testing ground for data \n - and charts.")
