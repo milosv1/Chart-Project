6d3fc67cbe8cea4e2e8acc9d292017925dc5c281
Milos <milosvuksanovic53@gmail.com>
2021-05-12 16:04:31 +1000

    Added main function with some imported items.

--- charts_redev.py
+++ charts_redev.py
@@ -1,2 +1,15 @@
 #the purpose of this file is to contain the new code
 #The goal of this file is to have code which is more clear and readable.
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+
+    print("first commit message..")
+
+
+
+if __name__ == "__main__":
+    main()
